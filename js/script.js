6d9cd60b76b4dde999fbfc08231b0c013cf3b8f2
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-07-07 09:26:55 -0300

    Desafios 2 e 3

--- js/script.js
+++ js/script.js
@@ -6,3 +6,32 @@ function reverseWords() {
     document.getElementById("output1").value = output;
 }
   
+function removeDuplicates() {
+          const inputText = document.getElementById("inputText2").value;
+          let uniqueChars = "";
+          for (let i = 0; i < inputText.length; i++) {
+            if (uniqueChars.indexOf(inputText[i]) === -1) {
+              uniqueChars += inputText[i];
+            }
+          }
+          document.getElementById("output2").value = uniqueChars;
+        }
+        
+        function longestPalindrome() {
+            var input = document.getElementById("inputText3").value;
+            var longest = "";
+            for (var i = 0; i < input.length; i++) {
+              for (var j = i + 1; j <= input.length; j++) {
+                var substr = input.substring(i, j);
+                if (isPalindrome(substr) && substr.length > longest.length) {
+                  longest = substr;
+                }
+              }
+            }
+            document.getElementById("output3").value = longest;
+          }
+          
+          function isPalindrome(str) {
+            var reversed = str.split("").reverse().join("");
+            return str === reversed;
+          }
