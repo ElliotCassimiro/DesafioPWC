c2b20fe04debb6d5aa3a6e3d261d6ed55ebfeca9
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-07-08 23:46:21 -0300

    Desafios 4 e 5 mais README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Manipulação de Strings
+
+Este é um projeto de manipulação de strings, onde são implementados cinco desafios de manipulação de strings. Cada desafio é abordado por uma função JavaScript correspondente.
+
+## Funcionalidades
+
+### Desafio 1 - Reverter Palavras
+
+Neste desafio, a ordem das palavras em uma frase é revertida, mantendo a ordem das próprias palavras.
+
+### Desafio 2 - Remover Letras Repetidas
+
+Neste desafio, todos os caracteres duplicados em uma string são removidos, mantendo apenas a primeira ocorrência de cada caractere.
+
+### Desafio 3 - Substring Palíndroma
+
+Neste desafio, a maior substring palíndroma em uma string é encontrada.
+
+### Desafio 4 - Capitalizar Frases
+
+Neste desafio, a primeira letra de cada frase em uma string é capitalizada.
+
+### Desafio 5 - Anagrama Palíndromo
+
+Neste desafio, verifica-se se uma string é um anagrama de um palíndromo.
+
+### Insira a string de entrada nos campos de texto correspondentes a cada desafio.
--- js/script.js
+++ js/script.js
@@ -35,3 +35,31 @@ function removeDuplicates() {
             var reversed = str.split("").reverse().join("");
             return str === reversed;
           }
+
+          function capitalizeSentences() {
+            const inputText = document.getElementById("inputText4").value;
+            const sentences = inputText.split(". ");
+            const capitalized = sentences.map(sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1));
+            const output = capitalized.join(". ");
+            document.getElementById("output4").value = output;
+          }
+          
+          function isAnagramPalindrome() {
+            const input = document.getElementById("inputText5").value;
+            const charCount = {};
+            
+            for (let i = 0; i < input.length; i++) {
+              const char = input[i];
+              charCount[char] = (charCount[char] || 0) + 1;
+            }
+            
+            let oddCount = 0;
+            for (const char in charCount) {
+              if (charCount[char] % 2 !== 0) {
+                oddCount++;
+              }
+            }
+            
+            const output = oddCount <= 1 ? "true" : "false";
+            document.getElementById("output5").value = output;
+          }
